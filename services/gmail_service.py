e6b4ab3a28db215ef85c81003eb7f79f9e2c7e77
Daniels <dorozcodaniel@gmail.com>
2026-07-30 17:22:51 +0000

    v4 y hacer lo de gmail

---
+++ services/gmail_service.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly"
+]
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+CREDENTIALS = BASE_DIR / "credentials" / "client_secret.json"
+
+TOKEN = BASE_DIR / "credentials" / "token.json"
+
+
+def autenticar():
+
+    creds = None
+
+    if TOKEN.exists():
+        creds = Credentials.from_authorized_user_file(str(TOKEN), SCOPES)
+
+    if not creds or not creds.valid:
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+
+            flow = InstalledAppFlow.from_client_secrets_file(
+                str(CREDENTIALS),
+                SCOPES
+            )
+
+            creds = flow.run_console()
+        TOKEN.write_text(creds.to_json())
+
+    return creds
+
+
+if __name__ == "__main__":
+
+    autenticar()
+
+    print("✅ Gmail conectado correctamente")
